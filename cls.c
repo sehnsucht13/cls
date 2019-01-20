6dfb77f7ff17c71a69fd3d36579b004e924c17b5
Yavor <ykonstantinov1@gmail.com>
2019-01-19 18:38:53 -0800

    Added a function to detect if a file is a directory

--- cls.c
+++ cls.c
@@ -6,7 +6,7 @@
 struct dirent **dirFile;
 int numOfFiles;
 
-int displayUpperDirs(const struct dirent *entry){
+int displayAlmostAll(const struct dirent *entry){
     char *sDot = ".";
     char *dDot = "..";
     if(strcmp(sDot, entry->d_name) == 0 || strcmp(dDot, entry->d_name) == 0){
@@ -17,20 +17,34 @@ int displayUpperDirs(const struct dirent *entry){
     }
 }
 
-int displayAllFiles(const struct dirent *entry){
+int displayAll(const struct dirent *entry){
     return 1;
+}
+
+int displayDirsOnly(const struct dirent *entry){
+    if(entry->d_type == DT_DIR){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
 void printColor(char *str, int colorNumber){
     printf("\033[1;38;5;%dm %s\n", colorNumber, str);
 }
 
-void printDirFiles(char *dirName, int (*f) (const struct dirent *entry), int recursive){
+void printDirFiles(const char *dirName, int (*f) (const struct dirent *entry), int recursive){
     numOfFiles = scandir(dirName, &dirFile, f, NULL);
     int currIndex = 0;
-    if (numOfFiles >=0 && recursive == 0){
+    if (numOfFiles >=0){
         for(currIndex; currIndex < numOfFiles; currIndex++){
+            if(dirFile[currIndex]->d_type == DT_DIR){
+                printColor(dirFile[currIndex]->d_name, 52);
+            }
+            else{
                 printColor(dirFile[currIndex]->d_name, 197);
+            }
         }
     }
     else{
@@ -49,7 +63,7 @@ void listCurrDir(){
 }
 
 void listDir(const char *dirName){
-    printDirFiles(dirName, NULL, 0);
+    printDirFiles(dirName, NULL , 0);
 }
 
 
