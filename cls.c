f75686289acdd9c108a3623802b41365c38a4e22
Yavor <ykonstantinov1@gmail.com>
2019-01-19 16:10:50 -0800

    Implemented the display of . and .. dirs and also filtering them from output.

--- cls.c
+++ cls.c
@@ -7,13 +7,23 @@ DIR *dirStream;
 struct dirent **dirFile;
 int numOfFiles;
 
-int displayDotFiles(const struct dirent *entry){
+int displayUpperDirs(const struct dirent *entry){
     char *sDot = ".";
     char *dDot = "..";
-
+    if(strcmp(sDot, entry->d_name) == 0 || strcmp(dDot, entry->d_name) == 0){
+        return 0;
+    }
+    else{
+        return 1;
+    }
 }
-void listDir(char *dirName){
-    numOfFiles = scandir(dirName, &dirFile, NULL, NULL);
+
+int displayAllFiles(const struct dirent *entry){
+    return 1;
+}
+
+void listDirFiles(char *dirName, int (*f) (const struct dirent *entry)){
+    numOfFiles = scandir(dirName, &dirFile, f, NULL);
     if (numOfFiles >=0){
         int currIndex = 0;
         for(currIndex; currIndex < numOfFiles; currIndex++){
@@ -28,8 +38,7 @@ void listDir(char *dirName){
 void listCurrDir(){
     printf("Listing current directory\n");
     char *currWorkDir = getcwd(NULL, 0);
-    listDir(currWorkDir);
-
+    listDirFiles(currWorkDir, NULL);
 }
 
 int main(int argc, char const *argv[])
